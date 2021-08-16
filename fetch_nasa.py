0c5f8b66a18c107c9777035602df485b1dcaf848
YungIshimura <83189636+YungIshimura@users.noreply.github.com>
2021-08-16 15:48:24 +0300

    Починил извлечение filename из url

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 
 def url_split(url):
+    url = urllib.parse.unquote(url)
     url_slpit = urllib.parse.urlsplit(url)
     path = url_slpit[2]
     return os.path.splitext(path)[1]
