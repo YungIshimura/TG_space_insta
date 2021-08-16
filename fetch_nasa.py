64124d124a5c9c439bbaa4ae1d9bf4528fa112cc
YungIshimura <83189636+YungIshimura@users.noreply.github.com>
2021-08-16 15:47:25 +0300

    Добавил проверку статуса ответа + поправил ссылку

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -18,12 +18,13 @@ def fetch_nasa_apod(apod_links):
         "end_date": "2016-02-02",
     }
     response = requests.get(nasa_apod_url,params=payload)
-    response.raise_for_status
+    response.raise_for_status()
     apod = response.json()
     for links in apod:
         apod_links.append(links["url"])
     for number, images in enumerate(apod_links, 1):
         response = requests.get(images)
+        response.raise_for_status()
         extension = url_split(images)
         with open(os.path.join(direc, "nasa apod"f'{number}{extension}'), "wb") as file:
             file.write(response.content)
@@ -34,17 +35,18 @@ def fetch_nasa_epic(epic_images):
         "api_key":"DEMO_KEY"
     }
     response = requests.get(nasa_epic_url,params=payload)
+    response.raise_for_status()
     epics = response.json()
     for epic in epics:
         adatetime = datetime.datetime.fromisoformat(epic["date"])
         formatted_date = adatetime.strftime("%Y/%m/%d")
         epic_images.append(epic["image"])
-    payload = {"api_key": nasa_api_key}
+    payload= {
+        "api_key":nasa_api_key
+        }
     for number, image in enumerate(epic_images, 1):
-        response = requests.get(
-            "https://api.nasa.gov/EPIC/archive/natural,"f"{'/'}{formatted_date}{'/png'}{'/'}{image}{'.png'}",
-            params=payload
-        )
+        response = requests.get("https://api.nasa.gov/EPIC/archive/natural"f"{'/'}{formatted_date}{'/png'}{'/'}{image}{'.png'}",params=payload)
+        response.raise_for_status()
         epic_link = response.url
         extension = url_split(epic_link)
         with open(os.path.join(direc, f'{"nasa epic "}{number}{extension}'), "wb") as file:
