874b9b5ac474fefed5b9da95af5c7a507c7cc725
YungIshimura <83189636+YungIshimura@users.noreply.github.com>
2021-08-17 11:42:51 +0300

    Поправил неймниг

--- send_message.py
+++ send_message.py
@@ -7,13 +7,13 @@ import time
 
 def send_message(chat_id):
     while True:
-        for images in listdir(direc):
-            bot.send_document(chat_id=chat_id, document=open(f"{direc}{'/'}{images}",'rb'))
+        for images in listdir(directory):
+            bot.send_document(chat_id=chat_id, document=open(f"{directory}{'/'}{images}",'rb'))
             time.sleep(86400)
 
 
 if __name__ == '__main__':
-    direc = input('Введите название папки, из которой будут отпраляться фото ')
+    directory = input('Введите название папки, из которой будут отпраляться фото ')
     load_dotenv()
     chat_id = os.getenv('CHAT_ID')
     tgbot_api_key = os.getenv('TGBOT_API_KEY')
