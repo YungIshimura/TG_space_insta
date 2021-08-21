30d7b4008b7ba335e02e7ef2f8a66e4b17c9fb81
YungIshimura <83189636+YungIshimura@users.noreply.github.com>
2021-08-21 14:35:58 +0300

    Убрал весь копипаст

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -2,7 +2,7 @@ import requests
 import os
 import datetime
 from dotenv import load_dotenv
-from url_split_and_dowland_images import url_split,download_image
+from url_split_and_dowland_images import url_split, download_image
 
 
 def fetch_nasa_apod(directory):
@@ -12,27 +12,22 @@ def fetch_nasa_apod(directory):
         "end_date": "2016-02-02",
     }
     nasa_apod_url = "https://api.nasa.gov/planetary/apod"
-    response = requests.get(nasa_apod_url,params=payload)
+    response = requests.get(nasa_apod_url, params=payload)
     response.raise_for_status()
     apod = response.json()
     apod_links = []
-    
     for links in apod:
         apod_links.append(links["url"])
-    for number, images in enumerate(apod_links, 1):
-        response = requests.get(images)
-        response.raise_for_status()
+    for number, image in enumerate(apod_links, 1):
         extension = url_split(images)
-        with open(os.path.join(directory, f"nasa apod{number}{extension}"), "wb") as file:
-            file.write(response.content)
+        path = (directory, f"nasa apod{number}{extension}")
+        download_image(path, image)
 
 
 def fetch_nasa_epic(directory):
-    payload= {
-        "api_key":nasa_api_key
-        }
+    payload = {"api_key": nasa_api_key}
     nasa_epic_url = "https://api.nasa.gov/EPIC/api/natural/date/2019-05-30"
-    response = requests.get(nasa_epic_url,params=payload)
+    response = requests.get(nasa_epic_url, params=payload)
     response.raise_for_status()
     epics = response.json()
     epic_images = []
@@ -41,17 +36,15 @@ def fetch_nasa_epic(directory):
         formatted_date = adatetime.strftime("%Y/%m/%d")
         epic_images.append(epic["image"])
     for number, image in enumerate(epic_images, 1):
-        response = requests.get(f"https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{image}.png",params=payload)
-        response.raise_for_status()
-        epic_link = response.url
-        extension = url_split(epic_link)
-        with open(os.path.join(directory, f"nasa epic{number}{extension}"), "wb") as file:
-            file.write(response.content)
+        nasa_epic_link = f"https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{image}.png"
+        extension = url_split(nasa_epic_link)
+        path = os.path.join(directory, f"nasa epic{number}{extension}")
+        download_image(path, nasa_epic_link, payload=payload)
 
 
 if __name__ == "__main__":
     directory = input("Введите название папки ")
-    os.makedirs(directory,exist_ok=False)
+    os.makedirs(directory, exist_ok=False)
     load_dotenv()
     nasa_api_key = os.getenv("NASA_API_KEY")
     fetch_nasa_apod(directory)
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,23 +1,21 @@
 import requests
 import os
 from dotenv import load_dotenv
-from url_split import url_split
+from url_split_and_dowland_images import url_split, download_image
+
 
 def fetch_spacex_last_launch(directory):
-    response = requests.get("https://api.spacexdata.com"f'{"/v3"}{"/launches"}')
+    response = requests.get("https://api.spacexdata.com" f'{"/v3"}{"/launches"}')
     response.raise_for_status()
     launches = response.json()
     images_links = launches[15]["links"]["flickr_images"]
-    for number, images in enumerate(images_links, 1):
-        extension = url_split(images)
-        response = requests.get(images)
-        response.raise_for_status()
-        with open(os.path.join(directory, f"spacex image{number}{extension}"), "wb") as file:
-            file.write(response.content)
+    for number, image in enumerate(images_links, 1):
+        extension = url_split(image)
+        path = os.path.join(directory, f"spacex image{number}{extension}")
+        download_image(path, image)
 
 
 if __name__ == "__main__":
     directory = input("Введите название папки ")
-    os.makedirs(directory,exist_ok=False)
+    os.makedirs(directory, exist_ok=False)
     fetch_spacex_last_launch(directory)
-    
