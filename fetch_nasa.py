fa26a3667ce9cee6a29542c2c2a589ac8034e618
YungIshimura <83189636+YungIshimura@users.noreply.github.com>
2021-08-27 17:14:10 +0300

    Изменил операции со списками

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -5,23 +5,21 @@ from dotenv import load_dotenv
 from get_file_extension_and_dowland_images import get_file_extension, download_image
 
 
-def fetch_nasa_apod(directory, nasa_api_key):
-    payload = {
-        "api_key": nasa_api_key,
-        "start_date": "2016-01-01",
-        "end_date": "2016-02-02",
-    }
-    nasa_apod_url = "https://api.nasa.gov/planetary/apod"
-    response = requests.get(nasa_apod_url, params=payload)
-    response.raise_for_status()
-    apod = response.json()
-    apod_links = []
-    for link in apod:
-        apod_links.append(links["url"])
-    for number, image in enumerate(apod_links, 1):
-        extension = get_file_extension(image)
-        path = (directory, f"nasa apod{number}{extension}")
-        download_image(path, image)
+# def fetch_nasa_apod(directory, nasa_api_key):
+#     payload = {
+#         "api_key": nasa_api_key,
+#         "start_date": "2016-01-01",
+#         "end_date": "2016-02-02",
+#     }
+#     nasa_apod_url = "https://api.nasa.gov/planetary/apod"
+#     response = requests.get(nasa_apod_url, params=payload)
+#     response.raise_for_status()
+#     apod = response.json()
+#     apod_links = [link["url"] for link in apod]
+#     for number, image in enumerate(apod_links, 1):
+#         extension = get_file_extension(image)
+#         path = os.path.join(directory, f"nasa apod{number}{extension}")
+#         download_image(path, image)
 
 
 def fetch_nasa_epic(directory, nasa_api_key):
@@ -30,22 +28,20 @@ def fetch_nasa_epic(directory, nasa_api_key):
     response = requests.get(nasa_epic_url, params=payload)
     response.raise_for_status()
     epics = response.json()
-    epic_images = []
-    for epic in epics:
-        adatetime = datetime.datetime.fromisoformat(epic["date"])
-        formatted_date = adatetime.strftime("%Y/%m/%d")
-        epic_images.append(epic["image"])
+    adatetime = [datetime.datetime.fromisoformat(epic["date"]) for epic in epics]
+    formatted_date = adatetime[0].strftime("%Y/%m/%d")
+    epic_images = [(epic["image"]) for epic in epics]
     for number, image in enumerate(epic_images, 1):
         nasa_epic_link = f"https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{image}.png"
-        extension = get_file_extension(image)
+        extension = get_file_extension(nasa_epic_link)
         path = os.path.join(directory, f"nasa epic{number}{extension}")
         download_image(path, nasa_epic_link, payload=payload)
-
+        
 
 if __name__ == "__main__":
     directory = input("Введите название папки ")
     os.makedirs(directory, exist_ok=False)
     load_dotenv()
     nasa_api_key = os.getenv("NASA_API_KEY")
-    fetch_nasa_apod(directory, nasa_api_key)
+    # fetch_nasa_apod(directory, nasa_api_key)
     fetch_nasa_epic(directory, nasa_api_key)
