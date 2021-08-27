f0b43863828422ea0391eb04c766c99ad6176486
YungIshimura <83189636+YungIshimura@users.noreply.github.com>
2021-08-27 13:10:01 +0300

    Отформатировал код

--- send_message.py
+++ send_message.py
@@ -5,10 +5,10 @@ import telegram
 import time
 
 
-def photo_publication(bot,directory,chat_id,tgbot_api_key):
+def photo_publication(bot, directory, chat_id, tgbot_api_key):
     while True:
         for images in listdir(directory):   
-             with open (f"{direc}/{images}","rb") as file:
+             with open(f"{direc}/{images}","rb") as file:
                  bot.send_document(chat_id, document=file)
                  time.slepp(86400)
 
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     chat_id = os.getenv("CHAT_ID")
     tgbot_api_key = os.getenv("TGBOT_API_KEY")
     bot = telegram.Bot(token=tgbot_api_key)
-    photo_publication(bot,directory,chat_id,tgbot_api_key)
+    photo_publication(bot, directory, chat_id, tgbot_api_key)
